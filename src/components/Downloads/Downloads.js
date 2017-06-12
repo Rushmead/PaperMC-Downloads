9b1f0593d7e7ceb32684a75cefbb1f77ae0b7838
Stuart <Rushmead@techie.com>
2017-06-12 19:15:45 +0100

    Bit more

--- src/components/Downloads/Downloads.js
+++ src/components/Downloads/Downloads.js
@@ -9,9 +9,7 @@ import * as downloadActions from '../../actions/downloads';
 class Downloads extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            rows: []
-        }
+        this.state = {}
     }
 
     render() {
@@ -19,15 +17,22 @@ class Downloads extends Component {
         <div className="container">
             <h2>Downloads</h2>
             <hr />
+            {this.props.rows.map(row =>
+                 <div className="row" key={this.props.rows.indexOf(row)}>
+                    {row.map(download => {
+                        return <div className="col-md-4" key={download}><Download version={download} key={download} /></div>
+                    })}
+                </div>
+            )}
         </div>
         );
     }
 }
 
-const mapStateToProps = (state, props) => {
-  return {
-       rows: state.rows,
-  };
+function mapStateToProps(state, props) {
+    return {
+        rows: state.rows
+    };
 }
 function mapDispatchToProps(dispatch) {
     return {
